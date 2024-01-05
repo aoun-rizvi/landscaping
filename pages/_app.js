ec82917bd171f9383b4a21930a8437fbef062bf8
aoun-rizvi <gh@aounrizvi.com>
2024-01-05 14:48:56 -0500

    Moved CSS under globals.css file. Using font locally now.

--- pages/_app.js
+++ pages/_app.js
@@ -16,66 +16,16 @@ const App = ({ Component, pageProps }) => {
         {/*====== Required meta tags ======*/}
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="description"
-          content="Landscaping, Gardening, Florists, Groundskeeping"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
+        <meta name="description" content="Landscaping Example Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         {/*====== Title ======*/}
-        <title>Gadden - Garden &amp; Landscaping React NextJS Template</title>
+        <title>Landscaping - Example</title>
         {/*====== Favicon Icon ======*/}
-        <link
-          rel="shortcut icon"
-          href="assets/images/favicon.ico"
-          type="image/png"
-        />
-        {/*====== Google Fonts ======*/}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        {/*====== Flaticon css ======*/}
-        <link
-          rel="stylesheet"
-          href="assets/fonts/flaticon/flaticon_gadden.css"
-        />
-        {/*====== FontAwesome css ======*/}
-        <link
-          rel="stylesheet"
-          href="assets/fonts/fontawesome/css/all.min.css"
-        />
-        {/*====== Bootstrap css ======*/}
-        <link
-          rel="stylesheet"
-          href="assets/vendor/bootstrap/css/bootstrap.min.css"
-        />
-        {/*====== magnific-popup css ======*/}
-        <link
-          rel="stylesheet"
-          href="assets/vendor/magnific-popup/dist/magnific-popup.css"
-        />
-        {/*====== Slick-popup css ======*/}
-        <link rel="stylesheet" href="assets/vendor/slick/slick.css" />
-        {/* <link rel="stylesheet" href="assets/vendor/slick/slick-theme.css" /> */}
+        <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png" />
+        {/*====== All Min css ======*/}
+        <link rel="stylesheet" href="assets/fonts/fontawesome/css/all.min.css" />
         {/*====== Jquery UI css ======*/}
-        <link
-          rel="stylesheet"
-          href="assets/vendor/jquery-ui/jquery-ui.min.css"
-        />
-        {/*====== Nice Select css ======*/}
-        <link
-          rel="stylesheet"
-          href="assets/vendor/nice-select/css/nice-select.css"
-        />
-        {/*====== Animate css ======*/}
-        <link rel="stylesheet" href="assets/vendor/animate.css" />
-        {/*====== Default css ======*/}
-        <link rel="stylesheet" href="assets/css/default.css" />
-        {/*====== Style css ======*/}
-        <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="stylesheet" href="assets/vendor/jquery-ui/jquery-ui.min.css" />
       </Head>
       {loading && <Preloader />}
       {!loading && <Component {...pageProps} />}
